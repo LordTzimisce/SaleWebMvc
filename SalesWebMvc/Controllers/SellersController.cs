98278ff073ec1fdfa5250d77aaa92917207d72e1
LordTzimisce <willams.trb@gmail.com>
2020-08-26 09:41:15 -0300

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -34,5 +34,30 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index)); //Redireciona para a página index => Dessa forma tbm funciona : return RedirectToAction("Index");
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id.Value == 0)
+            {
+                return NotFound(); //Retorna um notfound para a tela
+            }
+
+            var seller = _sellerService.FindById(id.Value);
+
+            if(seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
